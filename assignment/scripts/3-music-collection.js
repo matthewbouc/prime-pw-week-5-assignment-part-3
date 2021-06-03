b41ca28b81c37086b2e7312c99566c1c80d41089
Matthew Bouc <boucgm@gmail.com>
2021-06-02 21:04:57 -0500

    albums added and logged

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -3,15 +3,22 @@ console.log('***** Music Collection *****')
 const collection = [];
 
 const addToCollection = (title, artist, yearPublished) => {
-  let record = {
+  let album = {
   title: title,
   artist: artist,
   yearPublished: yearPublished
   }
-  collection.push(record);
-  return record;
+  collection.push(album);
+  return album;
 }
 
-console.log(addToCollection ('fish','nofish','1962'));
+console.log(addToCollection('fish','nofish','1962'));
+console.log(addToCollection('plenty o', 'nofish', '1961'));
+console.log(addToCollection('The Moon', 'Blue Phil', '1976'));
+console.log(addToCollection("Tailgatin'", 'Tayley Rich', '2017'));
+console.log(addToCollection('Boats on Water', 'Tayley Rich', '2015'));
+console.log(addToCollection('Jamming on these drums to release the frustration of a hard day at work as a lawyer', 'Kelly', '1993'));
+
+
 
 console.log(collection);
