dfc3d95a1c2d10692ecc70be99737deaefb3a35e
Matthew Bouc <boucgm@gmail.com>
2021-06-02 20:52:22 -0500

    Created addToCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,17 @@
 console.log('***** Music Collection *****')
+
+const collection = [];
+
+const addToCollection = (title, artist, yearPublished) => {
+  let record = {
+  title: title,
+  artist: artist,
+  yearPublished: yearPublished
+  }
+  collection.push(record);
+  return record;
+}
+
+console.log(addToCollection ('fish','nofish','1962'));
+
+console.log(collection);
